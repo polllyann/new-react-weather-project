2779dcae06fa7854752d513f72cb57f9a29f3313
Anna <108572062+polllyann@users.noreply.github.com>
2022-09-23 15:54:08 +0200

    added css

--- src/Weather.js
+++ src/Weather.js
@@ -6,28 +6,31 @@ export default function Weather() {
         <form>
             <div className="row">
                 <div className="col-9">
-            <input type="search" placeholder="Enter a city..." className="form-control" /></div>
+            <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on"/></div>
             <div className="col-3">
-            <input type="submit" value="Search"className="btn btn-primary" /></div>
+            <input type="submit" value="Search" className="btn btn-light w-100" /></div>
             </div>
         </form>
-        <h1>Oslo</h1>
+        <div className="row mt-3">
+            <div className="col-6">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/rain.png" 
+                alt="Rain"
+                />
+                <span className="temperature">6</span>
+                <span className="unit">°C</span>
+            <ul>
+                <li>Precipitation: 98%</li>
+                <li>Humidity: 96%</li>
+                <li>Wind: 21 km/h</li>
+                </ul>   
+            </div>
+            <div className="col-6 city">
+            <h1>Oslo</h1>
         <ul>
             <li>Friday 13:00</li>
             <li>Rain</li>
         </ul>
-        <div className="row">
-            <div className="col-6">
-                <img src="https://ssl.gstatic.com/onebox/weather/64/rain.png" alt="Rain" />
-                13°C
-            </div>
-            <div className="col-6">
-                <ul>
-                    <li>Precipitation: 98%</li>
-                    <li>Humidity: 96%</li>
-                    <li>Wind: 21 km/h</li>
-                </ul>
             </div>
         </div>
-      </div>
+        </div>
 }
