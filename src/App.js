eb7f74d0d17a9e6aa40219f82088ff8868e3d0be
Anna <108572062+polllyann@users.noreply.github.com>
2022-09-23 12:53:11 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
      <h1>Weather App</h1>
      <footer><a href="https://github.com/polllyann/new-react-weather-project" target="_blank" rel="noreferrer">Open-sourse code</a> by <a href="https://anna-p.netlify.app/" target="_blank" rel="noreferrer">Anna P.</a></footer>
+    </div>
     </div>
   );
 }
