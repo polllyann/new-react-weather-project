2d2d2e3e93c92288807bbe99b6881ec66ef6c426
Anna <108572062+polllyann@users.noreply.github.com>
2022-09-23 12:24:26 +0200

    removed unnecessary files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Weather App</h1>
+     <footer>Open-sourse code by <a href="https://anna-p.netlify.app/" target="_blank" rel="noreferrer">Anna P.</a></footer>
+    </div>
+  );
+}
+
+export default App;
